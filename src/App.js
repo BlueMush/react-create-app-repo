6ffb6515af7edad80efcb47d726ce2f31eda5284
DesktopMush <gusah0909@naver.com>
2022-02-25 17:39:59 +0900

    react components test

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
-import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React, { Component } from 'react';
+import TOC from "./components/TOC"
+import Subject from "./components/Subject"
+import Content from "./components/Content"
 import './App.css';
 
-function App() {
-  // 요청받은 정보를 담아줄 변수 선언
-  const [ testStr, setTestStr ] = useState('');
-
-  // 변수 초기화
-  function callback(str) {
-    setTestStr(str);
-  }
-
-  // 첫 번째 렌더링을 마친 후 실행
-  useEffect(
-      () => {
-        axios({
-            url: '/api/home',
-            method: 'GET'
-        }).then((res) => {
-            callback(res.data);
-        })
-      }, []
-  );
-
-  return (
+class App extends Component {
+  render() {
+    return (
       <div className="App">
-          <header className="App-header">
-              {testStr}
-          </header>
+        <Subject title="WEB" sub="World Wide Web"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Subject.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Subject extends Component {
+    render(){
+      return (
+        // 안에 쓸때 최상위 태그를 하나만 쓸 수 있음
+        <header>
+              <h1>{this.props.title}</h1>
+              {this.props.sub}
+          </header>
+      );
+    }
+}
+
+export default Subject;
