e6092cdfc0f6b20fa80b1183012e141398e25a02
DesktopMush <gusah0909@naver.com>
2022-02-25 19:52:47 +0900

    event 적용

--- src/App.js
+++ src/App.js
@@ -5,28 +5,37 @@ import Content from "./components/Content"
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      mode:"welcome",
-      subject:{title:"WEB", sub:"World Wide Web!"},
-      welcome:{title:'Welcome', desc:"Hello React!!"},
-      contents:[
-        {id:1, title:"HTML", desc:"Html is HyperText ..."},
-        {id:2, title:"CSS", desc:"CSS is for design ..."},
-        {id:3, title:"Naver", desc:"naver is good ..."}
+      mode: "read",
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: 'Welcome', desc: "Hello React!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "Html is HyperText ..." },
+        { id: 2, title: "CSS", desc: "CSS is for design ..." },
+        { id: 3, title: "Naver", desc: "naver is good ..." }
       ]
     }
   }
 
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
+        <Subject
+          title={this.state.subject.title}
           sub={this.state.subject.sub}></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc={this.state.contents.desc}></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
-    render (){
-        var list = [];
-        var data = this.props.data;
-        var i = 0;
-        for(i; i < data.length; i++){
-            list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
-        }
-        return (
-        <nav>
-            <ul>
-               {list}
-            </ul>
-        </nav>
-        );
+  render() {
+    var list = [];
+    var data = this.props.data;
+    var i = 0;
+    for (i; i < data.length; i++) {
+      list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
     }
+    return (
+      <nav>
+        <ul>
+          {list}
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default TOC;
