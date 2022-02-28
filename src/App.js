92ea00197756e20ae115d9c591d83ee7c2de7591
DesktopMush <gusah0909@naver.com>
2022-03-01 03:31:35 +0900

    react class형 함수형으로 변환 중

--- src/App.js
+++ src/App.js
@@ -6,68 +6,91 @@ import CreateContent from "./components/CreateContent"
 import Control from "./components/Control"
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: "read",
-      selected_content_id: 2,
-      subject: { title: "WEB", sub: "World Wide Web!" },
-      welcome: { title: 'Welcome', desc: "Hello React!!" },
-      contents: [
-        { id: 1, title: "HTML", desc: "Html is HyperText ..." },
-        { id: 2, title: "CSS", desc: "CSS is for design ..." },
-        { id: 3, title: "Naver", desc: "naver is good ..." }
-      ]
-    }
-  }
-
-  render() {
-    var _title, _desc, _article = null;
-    if (this.state.mode === 'welcome') {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if (this.state.mode === 'read') {
-      var i = 0;
-      while (i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
-    } else if (this.state.mode === 'create') {
-      _article = <CreateContent></CreateContent>
-    }
-
-    return (
-      <div className="App">
-        <Subject
-          title={this.state.subject.title}
-          sub={this.state.subject.sub}
-          onChangePage={function () {
-            this.setState({ mode: 'welcome' });
-          }.bind(this)}></Subject>
-
-        <TOC onChangePage={function (id) {
-          this.setState({
-            mode: 'read',
-            selected_content_id: Number(id)
-          });
-        }.bind(this)} data={this.state.contents}></TOC>
-        <Control onChangeMode={function (mode) {
-          this.setState({
-            mode: mode
-          });
-        }.bind(this)}></Control>
-        {_article}
-      </div>
-    );
+const content = {
+  welcome: {
+    title: "Welcome",
+    desc: "Welcome React!"
+  },
+  subject: {
+    title: "WEB",
+    sub: "world wide web"
   }
 }
+
+const mode = {
+  welcome: "welcome",
+  read: "read",
+  create: "create",
+  read: "read",
+  update: "update"
+}
+
+function App() {
+  return <TOC></TOC>
+}
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       mode: "read",
+//       selected_content_id: 2,
+//       subject: { title: "WEB", sub: "World Wide Web!" },
+//       welcome: { title: 'Welcome', desc: "Hello React!!" },
+//       contents: [
+//         { id: 1, title: "HTML", desc: "Html is HyperText ..." },
+//         { id: 2, title: "CSS", desc: "CSS is for design ..." },
+//         { id: 3, title: "Naver", desc: "naver is good ..." }
+//       ]
+//     }
+//   }
+
+//   render() {
+//     var _title, _desc, _article = null;
+//     if (this.state.mode === 'welcome') {
+//       _title = this.state.welcome.title;
+//       _desc = this.state.welcome.desc;
+//       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+//     } else if (this.state.mode === 'read') {
+//       var i = 0;
+//       while (i < this.state.contents.length) {
+//         var data = this.state.contents[i];
+//         if (data.id === this.state.selected_content_id) {
+//           _title = data.title;
+//           _desc = data.desc;
+//           break;
+//         }
+//         i = i + 1;
+//       }
+//       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+//     } else if (this.state.mode === 'create') {
+//       _article = <CreateContent></CreateContent>
+//     }
+
+//     return (
+//       <div className="App">
+//         <Subject
+//           title={this.state.subject.title}
+//           sub={this.state.subject.sub}
+//           onChangePage={function () {
+//             this.setState({ mode: 'welcome' });
+//           }.bind(this)}></Subject>
+
+//         <TOC onChangePage={function (id) {
+//           this.setState({
+//             mode: 'read',
+//             selected_content_id: Number(id)
+//           });
+//         }.bind(this)} data={this.state.contents}></TOC>
+//         <Control onChangeMode={function (mode) {
+//           this.setState({
+//             mode: mode
+//           });
+//         }.bind(this)}></Control>
+//         {_article}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
